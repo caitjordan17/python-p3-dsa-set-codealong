2e37f6420b4bd4a5c8d0b962a029fc107dcb919d
Caitlin Jordan <jordan.cait17@gmail.com>
2023-12-10 12:18:43 -0800

    completed lab

--- lib/MySet.py
+++ lib/MySet.py
@@ -1,3 +1,25 @@
 class MySet:
 
-    pass
+    def __init__(self, enumerable = []):
+        self.dictionary = {}
+        for value in enumerable:
+            self.dictionary[value] = True
+
+    def has(self, value):
+        return value in self.dictionary
+    
+    def add(self, value):
+        self.dictionary[value] = True
+        return self
+    
+    def delete(self, value):
+        self.dictionary.pop(value, None)
+        return self
+    
+    def size(self):
+        return len(self.dictionary)
+
+# set = MySet()
+# print("assigning set:",set.dictionary)
+# set = MySet([1,2,3,4,5,44,7,2])
+# print("set with vals:", set.dictionary)
